4bdf38c39ea0b5385b4b581fb8b05979cf8dc03e
Satish Nankani <snankani@microsoft.com>
2020-10-29 12:37:44 +0530

    skill update

--- serviceCatalog.ps1
+++ serviceCatalog.ps1
@@ -3,7 +3,7 @@ $scoreCards = @()
 $services = @("teams-integration", "slack-integration")
 
 function GetScoreCardsForService ([string]$service) {
-    $body = @"
+  $body = @"
 {
     service(name: "$($service)"){
     id,
@@ -32,41 +32,36 @@ function GetScoreCardsForService ([string]$service) {
 } 
 "@
 
-$header = @{
- "Authorization"="Bearer $($env:SERVICE_CATALOG_TOKEN)"
-} 
+  $header = @{
+    "Authorization" = "Bearer $($env:SERVICE_CATALOG_TOKEN)"
+  } 
 
 
-$resultJson = Invoke-RestMethod -Uri "https://catalog.githubapp.com/graphql" -Method 'Post' -Body $body -Headers $header 
+  $resultJson = Invoke-RestMethod -Uri "https://catalog.githubapp.com/graphql" -Method 'Post' -Body $body -Headers $header 
 
-return $resultJson
+  return $resultJson
 }
 
 
 
-Foreach ($service in $services)
-{
-   $scoreCard = GetScoreCardsForService $service
-   Foreach($iscoreCard in $scoreCard.data.service.scorecards.edges)
-   {
-        Foreach($iscorecardSummariesNodes in $iscoreCard.node.scorecardSummaries.nodes)
-        {
-            if($iscorecardSummariesNodes.score -lt $iscorecardSummariesNodes.maxScore)
-            {
-                $iscorecardSummariesNodes | Add-Member -NotePropertyName Status -NotePropertyValue "https://pluspng.com/img-png/red-cross-png-red-cross-png-file-2000.png"
-            }
-            else
-            {
-                $iscorecardSummariesNodes | Add-Member -NotePropertyName Status -NotePropertyValue "https://pluspng.com/img-png/green-tick-png-hd-green-tick-png-image-600.png"
-            }
-        }
-   }
-   $scoreCards = $scoreCards + $scoreCard
+Foreach ($service in $services) {
+  $scoreCard = GetScoreCardsForService $service
+  Foreach ($iscoreCard in $scoreCard.data.service.scorecards.edges) {
+    Foreach ($iscorecardSummariesNodes in $iscoreCard.node.scorecardSummaries.nodes) {
+      if ($iscorecardSummariesNodes.score -lt $iscorecardSummariesNodes.maxScore) {
+        $iscorecardSummariesNodes | Add-Member -NotePropertyName Status -NotePropertyValue "https://pluspng.com/img-png/red-cross-png-red-cross-png-file-2000.png"
+      }
+      else {
+        $iscorecardSummariesNodes | Add-Member -NotePropertyName Status -NotePropertyValue "https://pluspng.com/img-png/green-tick-png-hd-green-tick-png-image-600.png"
+      }
+    }
+  }
+  $scoreCards = $scoreCards + $scoreCard
 }
 
- $finalToReturn = ($scoreCards | ConvertTo-Json  -Depth 10 -Compress )
+$finalToReturn = ($scoreCards | ConvertTo-Json  -Depth 10 -Compress )
 
 
 
 
- $finalToReturn 
+$finalToReturn 
